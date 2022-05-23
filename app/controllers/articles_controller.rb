fddd01c27033986186ad7f0f3a33bc1c5165c959
Jhonny Tiscareño Ramirez <jhonnytisca@hotmail.com>
2022-05-23 14:18:51 -0500

    added articles update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,35 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created succesfully."
+      redirect_to @article #redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    #render plain: @article.inspect #params[:article]
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated succesfully."
+      redirect_to @article #redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+    #render plain: @article.inspect #params[:article]
+  end
+
 end
