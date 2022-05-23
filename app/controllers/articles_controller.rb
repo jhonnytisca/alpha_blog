78aa73f94d1a6d88b7062cccf1e9a6e602e1bfa7
Jhonny Tiscareño Ramirez <jhonnytisca@hotmail.com>
2022-05-23 15:44:59 -0500

    added articles delete and DRY code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
     #binding.break
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created succesfully."
       redirect_to @article #redirect_to article_path(@article)
@@ -28,8 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated succesfully."
       redirect_to @article #redirect_to article_path(@article)
     else
@@ -38,4 +36,18 @@ class ArticlesController < ApplicationController
     #render plain: @article.inspect #params[:article]
   end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
