f771ffd70aea95907cd606dcd274c48a962ce860
Jhonny Tiscareño Ramirez <jhonnytisca@hotmail.com>
2022-05-25 12:18:09 -0500

    added navbar and footer

---
+++ app/javascript/home.js
@@ -0,0 +1,2 @@
+document.getElementById('navigation_bar').className = "navbar navbar-expand-md px-4 navbar-dark fixed-top";
+document.getElementById('footer_bar').className = "py-4 text-white-50 position-absolute bottom-0 w-100";
