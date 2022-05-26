3daedda79265525b309c894ca3c5b707bea96735
Jhonny Tiscareño Ramirez <jhonnytisca@hotmail.com>
2022-05-26 01:58:42 -0500

    released Alpha Blog v1.0

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
+    flash[:notice] = "Article was deleted succesfully."
     redirect_to articles_path
   end
 
