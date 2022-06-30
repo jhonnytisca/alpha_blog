b674365a72e0276f0eba17b13c385472a988a956
Jhonny Tiscareño Ramirez <jhonnytisca@hotmail.com>
2022-06-30 18:22:48 -0500

    added users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def gravatar_for(user,options ={size:80})
+  def gravatar_for(user,options ={size:80, class:"rounded mx-auto d-block shadow"})
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block shadow my-5")
+    image_tag(gravatar_url, alt: user.username, class: options[:class])
   end
 end
