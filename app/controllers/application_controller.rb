9f588e24ad1310ee035d8bcccafe5b22d49df97a
Jhonny Tiscareño Ramirez <jhonnytisca@hotmail.com>
2022-07-01 10:43:19 -0500

    added logged in functionalities cleaned code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in? #Disponible en vistas
+
+  def current_user #Disponible en controladores
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] #Si el valor ya existe no lo asigna de nuevo
+  end
+
+  def logged_in?
+    !!current_user #Se convierte en un booleano
+  end
 end
